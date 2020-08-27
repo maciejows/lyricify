433d9a45a1e8bf429fb5edd383ea970a5aa2aff8
maciejows <owsianny.maciej@gmail.com>
2020-08-27 18:43:56 +0200

    Now trying until lyrics are fetched

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ const geniusApiBase = 'https://api.genius.com'
 
 app.get('/lyrics', (req, res) => {
     const query = req.query['search'];
-    console.log(query);
-    getData(`${geniusApiBase}/search?q=${query}`)
+    const slugged = slugify(query);
+    console.log("Called for: " + slugged);
+    getData(`${geniusApiBase}/search?q=${slugged}`)
         .then(data => {
             if (data.response.hits[0]) {
                 scrapeLyrics(data.response.hits[0].result.path)
@@ -47,10 +48,30 @@ async function getData(url=''){
 }
 
 async function scrapeLyrics(url){
-    const response = await fetch(`https://genius.com${url}`);
-    const text = await response.text();
-    const $ = cheerio.load(text);
-    const lyrics = $('.lyrics').text().trim();
-    console.log(lyrics);
+    let response = await fetch(`https://genius.com${url}`);
+    let text = await response.text();
+    let $ = cheerio.load(text);
+    let lyrics = $('.lyrics').text();
+    while(lyrics.length === 0) {
+        response = await fetch(`https://genius.com${url}`);
+        let text = await response.text();
+        $ = cheerio.load(text);
+        lyrics = $('.lyrics').text();
+    }
     return lyrics;
+}
+
+function slugify(text){
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+
+    return text.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '') // Trim - from end of text
 }
